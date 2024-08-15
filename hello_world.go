2d6ed179f11ff903cf6fad397a9c8ea65bee56c9
Sahil Shah <sahilshah2104@gmail.com>
2024-08-15 11:27:16 +0530

    Day 1 Commits

--- hello_world.go
+++ hello_world.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	myutil "tutorials/utils"
+)
 
 func main() {
 	fmt.Println("Hello World in GO!")
+	myutil.PrintMessage("Hello World from myutil")
 }
